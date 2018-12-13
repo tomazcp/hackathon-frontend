4361e01429e383091ca986964bf14d4e9dc8b52a
Tomás <tomaspinto@protonmail.ch>
2018-12-13 18:47:46 +0000

    register js

--- js/register.js
+++ js/register.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-    console.log('lets roll');
-})
+    
+    const registerForm = $('#registerForm');
+    const submitBtn = $('#registerBtn');
+    const inputs = $('#registerForm :input').not('#registerBtn');
+
+    const processForm = () => {
+        //registerForm.forEach()
+    }
+
+    submitBtn.click(function(e) {
+        e.preventDefault();
+        inputs.each(function() {
+            
+        })
+    });
+
+});
