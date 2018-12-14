0c5af0723864f0fed9107da29e5cb57e00527a89
Tomás <tomaspinto@protonmail.ch>
2018-12-14 06:07:41 +0000

    edit form

--- js/profile.js
+++ js/profile.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
     const imgAvatar = $('img.avatar');
     const nameTag = $('p.patient-name');
     const emailTag = $('p.patient-email');
+    const sidenavEdit = $('.sidenav-edit');
+    const sidenavProfile = $('.sidenav-profile');
+    const submitBtn = $('#editBtn');
+    const profile = $('.profile');
+    const formDiv = $('.form');
+    const inputs = $('#editProfile :input').not('#editBtn');
     const avatars = { male: '../img/male_avatar.png', female: '../img/female_avatar.png' };
     
     function getPatient(callback) {
@@ -24,9 +30,39 @@ $(document).ready(function() {
         imgAvatar.attr('src', avatars[patient.gender]);
         nameTag.text(patient.name);
         emailTag.text(patient.email);
+        populateForm(patient);
+        formDiv.hide();
     }
+
+    function populateForm(patient) {
+        inputs.each(function() {
+            this.value = patient[this.name];
+        });      
+    }
+
+    submitBtn.click(function(e) {
+        e.preventDefault();
+        let form = {};
+
+        input.each(function() {
+            form[$(this).attr('name')] = $(this).val();
+        });
+    });
+
+    sidenavEdit.click(function(e) {
+        e.preventDefault();
+        formDiv.show();
+        profile.hide();
+    });
+
+    sidenavProfile.click(function(e) {
+        e.preventDefault();
+        formDiv.hide();
+        profile.show();
+    });
 
 
     getPatient(onData);
+
 
 });
