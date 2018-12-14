7d2f8dd5b743bf258fdb01ce2ff4fbd9e1381a5c
Tomás <tomaspinto@protonmail.ch>
2018-12-14 03:13:05 +0000

    redirecting after registration

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+
+    const patientUrl = 'http://192.168.1.29:8080/hackathon/api/auth/profile';
     
+    function getPatient(callback) {
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: 'GET',
+            url: patientUrl,
+            dataType: 'json',
+            async: true,
+            success: callback
+        });
+    }
+
+    function onData(data) {
+        console.log(data)
+    }
+
+
+    getPatient(onData);
+
 });
